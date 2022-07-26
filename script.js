8c92d454eda5bfd65b7e3b0293b20ba217e42cf3
Ayavuya Genu <ayagenu@gmail.com>
2022-07-27 00:21:42 +0200

    simple interest calculator finished, just need to fix the math formula

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+   let p = document.getElementById("principal").value;
+   let r = document.getElementById("rate").value;
+   let y = document.getElementById("years").value;
     
+    let i = p + (1+r)**(y);
+    console.log(i);
+    document.getElementById("result").innerHTML = 'if your principal amount is '+ p.toString() + 'at a rate of ' + r.toString() + '% for ' + y.toString() + 'years, your interest is equal to ' + i.toString();  
+    
+}
+
+function slider_value(val)
+{
+    document.getElementById("rate_val").innerHTML = val.toString() + "%" ;
 }
         
